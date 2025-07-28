5013dab884946dc0dca8366bd5bf61bb5b35bd07
dracoDevs <67403295+dracoDevs@users.noreply.github.com>
2025-07-28 00:10:14 -0400

    commit: added another example for actually modifying request with Hijacking

--- examples/hijack.go
+++ examples/hijack.go
@@ -46,3 +46,52 @@ func TestHijackRequests() error {
 
 	return nil
 }
+
+func TestHijackModifyRequest() error {
+	chromePath, chromeInstalled := launcher.LookPath()
+	if !chromeInstalled {
+		return fmt.Errorf("Chrome not found in PATH")
+	}
+	b := browser.GreenLight(chromePath, false, "https://example.com")
+
+	if err := b.SendCommandWithoutResponse("Network.enable", nil); err != nil {
+		return fmt.Errorf("failed to enable network: %v", err)
+	}
+
+	b.SendCommandWithoutResponse("Network.setRequestInterception", map[string]interface{}{
+		"patterns": []map[string]interface{}{{"urlPattern": "*"}},
+	})
+
+	go func() {
+		for {
+			event := b.Listen()
+			if event.Method == "Network.requestIntercepted" {
+				reqID, ok := event.Params["interceptionId"].(string)
+				if !ok {
+					continue
+				}
+				headers := map[string]interface{}{
+					"X-My-Header": "ModifiedByHijack",
+				}
+				// Continue the request with modified headers
+				err := b.SendCommandWithoutResponse("Network.continueInterceptedRequest", map[string]interface{}{
+					"interceptionId": reqID,
+					"headers":        headers,
+				})
+				if err != nil {
+					log.Printf("Failed to continue intercepted request: %v", err)
+				} else {
+					log.Printf("Modified and continued request: %s", reqID)
+				}
+			}
+		}
+	}()
+
+	page := b.NewPage()
+	page.Goto("https://example.com")
+	page.YellowLight(5000000)
+
+	// b.RedLight()
+
+	return nil
+}
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	examples.TestHijackRequests()
+	examples.TestHijackModifyRequest()
 }
